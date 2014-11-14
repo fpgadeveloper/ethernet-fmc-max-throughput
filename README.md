120cadcc820bfda149f4889b25e4eba081c60758
Jeff Johnson <jeff@fpgadeveloper.com>
2014-11-14 14:26:26 -0500

    now forces errors every iteration of the loop added comments

--- README.md
+++ README.md
@@ -6,25 +6,30 @@ packet generator/checker to demonstrate maximum throughput.
 
 ### Description
 
-This project is used for testing the Ethernet FMC (ethernetfmc.com) at
+This project is used for testing the Ethernet FMC (http://ethernetfmc.com) at
 maximum throughput. The design contains 4 AXI Ethernet blocks and 4
 hardware packet generator/checkers. The software application sets up
 the MACs in promiscuous mode which allows them to pass through all
-packets, regardless of their destination MAC address.
+packets, regardless of their destination MAC address. It also sets them
+up to receive the FCS (checksum) from the user design, rather than
+calculating and inserting it itself.
+
+### Background
 
 In order to test an Ethernet device at maximum throughput (back-to-back
-packets at 1Gbps) it is generally difficult to use a PC to generate the
-packets and verify received packets, because a PC typically has too many
-overheads which create a delay between consecutive packets. For this
-reason, this design uses four hardware packet generator/checkers that
-are implemented in the FPGA. These generator/checkers drive the AXI
-Ethernet cores (the MACs) with a continuous stream of packets. They also
-verify the stream of packets received from the MACs and count the bit
-error rate and lost packets. In order to test the Ethernet FMC using
-this design, you need to use an Ethernet cable to loopback ports 0 and
-1, and ports 2 and 3. Software registers in the generator/checker IP
-core allow you to read the bit error rate and packet loss from the
-software application running on the ARM.
+packets at 1Gbps), one could setup the MACs to loopback to each other
+and then send packets to each port from an external source such as a PC
+which could compare the returned packets to the sent ones. However, it
+is generally difficult to use a PC Ethernet port at full throughput,
+because a PC typically has too many overheads which create a delay
+between consecutive packets. For this reason, this design uses four
+hardware packet generator/checkers that are implemented in the FPGA.
+These generator/checkers drive the AXI Ethernet cores (the MACs) with a
+continuous stream of packets. They also verify the stream of packets
+received from the MACs and count the bit error rate. In practice, any
+bit errors that affect the system will result in dropped packets at the
+receiving MAC, therefore we use the software application to poll the MACs
+and count the rejected frames.
 
 This design is actually used as a production test for the Ethernet FMC
 because it places maximum stress on the PHYs, which forces the maximum
@@ -34,6 +39,10 @@ need to communicate over Ethernet with another FPGA or an Ethernet
 device that can support the high throughput.
 
 ### Requirements
+
+In order to test the Ethernet FMC using this design, you need to use an
+Ethernet cable to loopback ports 0 and 2, and ports 1 and 3.
+You will also need the following:
 
 * Vivado 2014.2
 * ZedBoard
--- SDK/test_app/src/test_app.c
+++ SDK/test_app/src/test_app.c
@@ -10,6 +10,39 @@
  * Marvell PHYs on the Ethernet FMC to autonegotiate the
  * link speed and disable MAC address filtering.
  *
+ * The main loop does the following:
+ *  - force a bit error into a transmitted frame on all ports
+ *  - poll the rejected frame interrupt flag for about 1 second
+ *  - increment counters when dropped frames are detected
+ *  - display the value of the counters
+ *
+ * The console will display the dropped frame counts for all
+ * ports about once a second. The values should be incrementing
+ * by one for each reading. In normal operation, the value of
+ * the counter for each port should be the same which indicates
+ * that there have been no dropped packets besides those in
+ * which an error was forced by the Ethernet packet generator.
+ *
+ * Example (normal) output:
+ *
+ * Dropped frames (P0,P1,P2,P3):    1     1     1     1
+ * Dropped frames (P0,P1,P2,P3):    2     2     2     2
+ * Dropped frames (P0,P1,P2,P3):    3     3     3     3
+ * Dropped frames (P0,P1,P2,P3):    4     4     4     4
+ * Dropped frames (P0,P1,P2,P3):    5     5     5     5
+ * Dropped frames (P0,P1,P2,P3):    6     6     6     6
+ * Dropped frames (P0,P1,P2,P3):    7     7     7     7
+ *
+ * Example output where a frame was lost on port 2:
+ *
+ * Dropped frames (P0,P1,P2,P3):    1     1     1     1
+ * Dropped frames (P0,P1,P2,P3):    2     2     2     2
+ * Dropped frames (P0,P1,P2,P3):    3     3     4     3
+ * Dropped frames (P0,P1,P2,P3):    4     4     5     4
+ * Dropped frames (P0,P1,P2,P3):    5     5     6     5
+ * Dropped frames (P0,P1,P2,P3):    6     6     7     6
+ * Dropped frames (P0,P1,P2,P3):    7     7     8     7
+ *
  */
 
 #include <stdio.h>
@@ -17,8 +50,7 @@
 #include "platform.h"
 #include "ethfmc_axie.h"
 
-#define MAX_DELAY 0x00640000
-
+// Pointers to the Ethernet traffic generators
 Xuint32 *eth_pkt_gen_0_p = (Xuint32 *)XPAR_ETH_TRAFFIC_GEN_0_S_AXI_BASEADDR;
 Xuint32 *eth_pkt_gen_1_p = (Xuint32 *)XPAR_ETH_TRAFFIC_GEN_1_S_AXI_BASEADDR;
 Xuint32 *eth_pkt_gen_2_p = (Xuint32 *)XPAR_ETH_TRAFFIC_GEN_2_S_AXI_BASEADDR;
@@ -38,18 +70,7 @@ int main()
 	unsigned char mac_ethernet_address[] =
 	{ 0x00, 0x0a, 0x35, 0x00, 0x01, 0x02 };
 
-
 	init_platform();
-
-	// Set max delay to
-	reg = (*eth_pkt_gen_0_p) & 0x0000FFFF;
-	*eth_pkt_gen_0_p = reg | MAX_DELAY;
-	reg = (*eth_pkt_gen_1_p) & 0x0000FFFF;
-	*eth_pkt_gen_1_p = reg | MAX_DELAY;
-	reg = (*eth_pkt_gen_2_p) & 0x0000FFFF;
-	*eth_pkt_gen_2_p = reg | MAX_DELAY;
-	reg = (*eth_pkt_gen_3_p) & 0x0000FFFF;
-	*eth_pkt_gen_3_p = reg | MAX_DELAY;
 
 	xil_printf("Ethernet Port 0:\n\r");
 	EthFMC_init_axiemac(XPAR_AXIETHERNET_0_BASEADDR,mac_ethernet_address);
@@ -60,48 +81,58 @@ int main()
 	xil_printf("Ethernet Port 3:\n\r");
 	EthFMC_init_axiemac(XPAR_AXIETHERNET_3_BASEADDR,mac_ethernet_address);
 
-	// Reset counters
-	reg = *eth_pkt_gen_0_p;
-	*eth_pkt_gen_0_p = reg | 0x00000001;
-	reg = *eth_pkt_gen_1_p;
-	*eth_pkt_gen_1_p = reg | 0x00000001;
-	reg = *eth_pkt_gen_2_p;
-	*eth_pkt_gen_2_p = reg | 0x00000001;
-	reg = *eth_pkt_gen_3_p;
-	*eth_pkt_gen_3_p = reg | 0x00000001;
-	reg = *eth_pkt_gen_0_p;
-	*eth_pkt_gen_0_p = reg & ~(0x00000001);
-	reg = *eth_pkt_gen_1_p;
-	*eth_pkt_gen_1_p = reg & ~(0x00000001);
-	reg = *eth_pkt_gen_2_p;
-	*eth_pkt_gen_2_p = reg & ~(0x00000001);
-	reg = *eth_pkt_gen_3_p;
-	*eth_pkt_gen_3_p = reg & ~(0x00000001);
-
 	// Reset the reject frame interrupt flags
 	XAxiEthernet_WriteReg(XPAR_AXIETHERNET_0_BASEADDR,XAE_IS_OFFSET,XAE_INT_RXRJECT_MASK);
 	XAxiEthernet_WriteReg(XPAR_AXIETHERNET_1_BASEADDR,XAE_IS_OFFSET,XAE_INT_RXRJECT_MASK);
 	XAxiEthernet_WriteReg(XPAR_AXIETHERNET_2_BASEADDR,XAE_IS_OFFSET,XAE_INT_RXRJECT_MASK);
 	XAxiEthernet_WriteReg(XPAR_AXIETHERNET_3_BASEADDR,XAE_IS_OFFSET,XAE_INT_RXRJECT_MASK);
 
-	// Delay
-	for(i=0; i<100000000; i++){}
-
-	// Force an error
-	reg = *eth_pkt_gen_0_p;
-	*eth_pkt_gen_0_p = reg | 0x00000002;
-	// Delay
-	for(i=0; i<100000; i++){}
-	reg = *eth_pkt_gen_0_p;
-	*eth_pkt_gen_0_p = reg & ~(0x00000002);
-
-
+	// Reset the dropped frame counters
 	dropped_frames_0 = 0;
 	dropped_frames_1 = 0;
 	dropped_frames_2 = 0;
 	dropped_frames_3 = 0;
 
 	while (1) {
+		/* Force an error to be sent by all ports
+		 * --------------------------------------
+		 * The following register writes will make the Ethernet
+		 * traffic generator/checker force a single bit error into
+		 * a single transmitted Ethernet frame. The bit error
+		 * results in a rejected (dropped) frame at the receiving
+		 * end (whichever port that may be and it depends on how
+		 * the ports are looped back to each other).
+		 * The purpose of forcing an error like this is to ensure
+		 * that our method for counting dropped frames is actually
+		 * working.
+		 */
+
+		reg = *eth_pkt_gen_0_p;
+		*eth_pkt_gen_0_p = reg | 0x00000002;
+		reg = *eth_pkt_gen_1_p;
+		*eth_pkt_gen_1_p = reg | 0x00000002;
+		reg = *eth_pkt_gen_2_p;
+		*eth_pkt_gen_2_p = reg | 0x00000002;
+		reg = *eth_pkt_gen_3_p;
+		*eth_pkt_gen_3_p = reg | 0x00000002;
+		// Reset the force error
+		reg = *eth_pkt_gen_0_p;
+		*eth_pkt_gen_0_p = reg & ~(0x00000002);
+		reg = *eth_pkt_gen_1_p;
+		*eth_pkt_gen_1_p = reg & ~(0x00000002);
+		reg = *eth_pkt_gen_2_p;
+		*eth_pkt_gen_2_p = reg & ~(0x00000002);
+		reg = *eth_pkt_gen_3_p;
+		*eth_pkt_gen_3_p = reg & ~(0x00000002);
+
+		/* Poll for dropped packets and increment counters
+		 * -----------------------------------------------
+		 * This loop will repeatedly poll the rejected frame
+		 * interrupt flag of the AXI Ethernet IP. If the flag
+		 * is asserted, the dropped frame counter for that
+		 * port is incremented and the interrupt flag is
+		 * cleared by writing a 1 to it.
+		 */
 		for(i=0; i<1000000; i++){
 			// Read the interrupt status register
 			reg = XAxiEthernet_ReadReg(XPAR_AXIETHERNET_0_BASEADDR,XAE_IS_OFFSET);
@@ -136,8 +167,17 @@ int main()
 				dropped_frames_3++;
 			}
 		}
-		// Read from the bit error register
-		xil_printf("- Dropped packets: %8d %8d %8d %8d\n\r",
+		/* Display the dropped frame counter values
+		 * ----------------------------------------
+		 * Using good Ethernet cables and an environment with low EMI,
+		 * there should be no bit errors affecting the links between
+		 * the ports. In this case, there should not be any dropped
+		 * frames apart from those that are forced each iteration of
+		 * the main loop. Thus you should expect all counters to have
+		 * the same value always.
+		 *
+		 */
+		xil_printf("Dropped frames (P0,P1,P2,P3): %8d %8d %8d %8d\n\r",
 					dropped_frames_0,dropped_frames_1,dropped_frames_2,dropped_frames_3);
 	}
 
