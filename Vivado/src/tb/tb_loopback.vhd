bf6df3f0e832801b5eefec26a843e8b5ac040661
Jeff Johnson <jeff@fpgadeveloper.com>
2016-01-19 20:42:50 -0500

    added the TX to RX loopback

--- Vivado/src/tb/tb_loopback.vhd
+++ Vivado/src/tb/tb_loopback.vhd
@@ -1,5 +1,6 @@
 --------------------------------------------------------------------------------
--- Testbench for the Slave AXI interface
+-- RTL Testbench for the Ethernet Traffic Generator IP
+-- Opsero Electronic Design Inc.
 --------------------------------------------------------------------------------
 
 library ieee;
@@ -54,9 +55,6 @@ architecture tb of tb_loopback is
 
   signal end_of_simulation : boolean := false;
 
-  --signal force_error : std_logic := '0';
-  --signal force_drop  : std_logic := '0';
-  
 begin
 
   -----------------------------------------------------------------------
@@ -65,20 +63,20 @@ begin
 
 design_2_wrapper_inst : entity work.design_2_wrapper
 	port map (
-      ap_clk => ap_clk,
-      init_done => init_done,
-      reset_n => reset_n,
-      init_start => init_start,
-      s_axis_rxd_tdata(31 downto 0) => s_axis_rxd_tdata(31 downto 0),
-      s_axis_rxd_tkeep(3 downto 0) => s_axis_rxd_tkeep(3 downto 0),
-      s_axis_rxd_tlast(0) => s_axis_rxd_tlast(0),
-      s_axis_rxd_tready => s_axis_rxd_tready,
-      s_axis_rxd_tvalid => s_axis_rxd_tvalid,
-      s_axis_rxs_tdata(31 downto 0) => s_axis_rxs_tdata(31 downto 0),
-      s_axis_rxs_tkeep(3 downto 0) => s_axis_rxs_tkeep(3 downto 0),
-      s_axis_rxs_tlast(0) => s_axis_rxs_tlast(0),
-      s_axis_rxs_tready => s_axis_rxs_tready,
-      s_axis_rxs_tvalid => s_axis_rxs_tvalid,
+    ap_clk => ap_clk,
+    init_done => init_done,
+    reset_n => reset_n,
+    init_start => init_start,
+    s_axis_rxd_tdata(31 downto 0) => s_axis_rxd_tdata(31 downto 0),
+    s_axis_rxd_tkeep(3 downto 0) => s_axis_rxd_tkeep(3 downto 0),
+    s_axis_rxd_tlast(0) => s_axis_rxd_tlast(0),
+    s_axis_rxd_tready => s_axis_rxd_tready,
+    s_axis_rxd_tvalid => s_axis_rxd_tvalid,
+    s_axis_rxs_tdata(31 downto 0) => s_axis_rxs_tdata(31 downto 0),
+    s_axis_rxs_tkeep(3 downto 0) => s_axis_rxs_tkeep(3 downto 0),
+    s_axis_rxs_tlast(0) => s_axis_rxs_tlast(0),
+    s_axis_rxs_tready => s_axis_rxs_tready,
+    s_axis_rxs_tvalid => s_axis_rxs_tvalid,
     m_axis_txc_tdata(31 downto 0) => m_axis_txc_tdata(31 downto 0),
     m_axis_txc_tlast(0) => m_axis_txc_tlast(0),
     m_axis_txc_tready => m_axis_txc_tready,
@@ -113,76 +111,45 @@ design_2_wrapper_inst : entity work.design_2_wrapper
   end process clock_x_gen;
 
   -----------------------------------------------------------------------
+  -- TX-RX loopback
+  -----------------------------------------------------------------------
+  s_axis_rxd_tdata <= m_axis_txd_tdata;
+  s_axis_rxd_tkeep <= m_axis_txd_tkeep;
+  s_axis_rxd_tlast(0) <= m_axis_txd_tlast(0);
+  s_axis_rxd_tvalid <= m_axis_txd_tvalid;
+  s_axis_rxs_tdata <= m_axis_txc_tdata;
+  s_axis_rxs_tkeep <= m_axis_txc_tkeep;
+  s_axis_rxs_tlast(0) <= m_axis_txc_tlast(0);
+  s_axis_rxs_tvalid <= m_axis_txc_tvalid;
+  m_axis_txc_tready <= s_axis_rxs_tready;
+  m_axis_txd_tready <= s_axis_rxd_tready;
+  
+  -----------------------------------------------------------------------
   -- Generate inputs
   -----------------------------------------------------------------------
 
   stimuli : process
     variable bytenum : unsigned(7 downto 0);  -- byte number
   begin
-  -- leave RXS and RXD grounded
-    s_axis_rxd_tdata(31 downto 0) <= (others => '0');
-    s_axis_rxd_tkeep(3 downto 0) <= (others => '1');
-    s_axis_rxd_tlast(0) <= '0';
-    s_axis_rxd_tvalid <= '0';
-    s_axis_rxs_tdata(31 downto 0) <= (others => '0');
-    s_axis_rxs_tkeep(3 downto 0) <= (others => '1');
-    s_axis_rxs_tlast(0) <= '0';
-    s_axis_rxs_tvalid <= '0';
-
-  
-    m_axis_txc_tready <= '0';
-    m_axis_txd_tready <= '0';
     init_start <= '0';
---    start_traffic_gen <= '0';    
     reset_n <= '0';
 
     -- Drive inputs T_HOLD time after rising edge of clock
     wait until rising_edge(ap_clk);
     wait for T_HOLD;
 
+    -- Release reset
     wait for T_RST;
     reset_n <= '1';
     
-    -- start AXI register initialization
+    -- Start AXI software register initialization
     init_start <= '1';
     
+    -- Wait until software registers are initialized
     wait until init_done = '1';
---    wait for CLOCK_PERIOD;
     init_start <= '0';
---    start_traffic_gen <= '1';    
     
-    --wait until m_axis_txc_tdata = X"A0000000";
-    wait for CLOCK_PERIOD*3;
-    
-    for cycle in 0 to 5 loop
-      -- Ready for receive control data
-      m_axis_txc_tready <= '1';
-      m_axis_txd_tready <= '1';
-      --wait until txc_tdata = X"A0000000";
-      --wait for CLOCK_PERIOD*2;
-      --m_axis_txc_tready <= '0';
-      --wait for CLOCK_PERIOD*4;
-      --m_axis_txc_tready <= '1';
-      
-      -- When last control word is sent:
-      -- * deassert TXC tready
-      -- * assert TXD tready
-      wait until m_axis_txc_tlast(0) = '1';
-      wait for CLOCK_PERIOD;
-      m_axis_txc_tready <= '0';
-      wait for CLOCK_PERIOD*3;
-      m_axis_txd_tready <= '1';
-      
-      -- When last data word is sent:
-      -- * deassert TXD tready
-      -- * assert TXC tready
-      wait until m_axis_txd_tlast(0) = '1';
-      wait for CLOCK_PERIOD;
-      m_axis_txd_tready <= '0';
-      wait for CLOCK_PERIOD*5;
-      --m_axis_txc_tready <= '1';
-    end loop;
-    
+    -- Allow some time for packets to flow
     wait for CLOCK_PERIOD*500;
     
     -- End of test
